81e99ec831c9aad1826f0d19a9b3303234ce287d
jorge.carbajal <jorge.carbajal@dish.com.mx>
2019-07-20 20:17:15 -0500

    Ionic app-angular

---
+++ src/app/pages/agregar/agregar.page.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { DeseosService } from '../../services/deseos.service';
+import { ActivatedRoute } from '@angular/router';
+import { Lista } from '../../models/lista.model';
+import { ListaItem } from '../../models/lista-item.model';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.page.html',
+  styleUrls: ['./agregar.page.scss'],
+})
+export class AgregarPage implements OnInit {
+
+  lista: Lista;
+  nombreItem = '';
+
+  constructor( private deseosService: DeseosService,
+               private route: ActivatedRoute ) {
+    const listaId = this.route.snapshot.paramMap.get('listaId');
+
+    this.lista = this.deseosService.obtenerLista(listaId);
+   }
+
+  ngOnInit() {
+  }
+
+  agregarItem() {
+    if ( this.nombreItem.length === 0) {
+      return;
+    }
+    console.log(this.nombreItem);
+
+    const nuevoItem = new ListaItem(this.nombreItem);
+    this.lista.items.push(nuevoItem);
+    this.nombreItem = '';
+    this.deseosService.guardarStorage();
+  }
+
+}
